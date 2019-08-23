a224d01c503557711a315d62cc8c2c4014882fef
birdday <22bday@gmail.com>
2019-08-23 15:10:02 -0400

    Removed import * and replaced with appropriate functions. Removed bulky comment following import.

--- execute_process_mass_data.py
+++ execute_process_mass_data.py
@@ -6,41 +6,23 @@
 import sys
 import pandas as pd
 from datetime import datetime
-from mof_array.pmf.process_mass_data import *
-# List of Functions and Arguments in process_mass_data:
-#     read_data_as_dict(filename)
-#           --> return list(output_data)
-#     write_data_as_tabcsv(filename, data)
-#           --> return(writer)
-#     yaml_loader(filepath)
-#           --> return(data)
-#     import_experimental_data(exp_results_import, mof_list, mof_densities, gases)
-#           --> return(exp_results_full, exp_results_mass, exp_mof_list)
-#     import_simulated_data(sim_results_import, mof_list, mof_densities, gases)
-#           --> return(sim_results_full)
-#     add_random_gas(gases, comps, num_mixtures)
-#           --> NOT FUCTIONAL CURRENTLY
-#     calculate_pmf(exp_results_full, sim_results_full, mof_list, stdev, mrange)
-#           --> return(element_pmf_results)
-#     caclulate_array_pmf(mof_array, element_pmf_results)
-#           --> return(single_array_pmf_results)
-#     calculate_all_arrays(mof_list, num_mofs, element_pmf_results, gases)
-#           --> return(mof_array_list, all_array_pmf_results)
-#     create_bins(gases, num_bins, mof_list, element_pmf_results)
-#           --> return(bins)
-#     bin_compositions(gases, bins, list_of_arrays, all_array_pmf_results)
-#           --> return(binned_probabilities_sum, binned_probabilities_max)
-#     plot_binned_pmf_array(gases, list_of_arrays, bins, binned_probabilities)
-#           --> returns a set of plots to the folder 'figures'
-#     save_array_pmf_data(gases, list_of_arrays, bins, binned_probabilities)
-#           --> returns a set of csv files to the folder 'saved_array_pmfs'
-#     save_element_pmf_data(element_pmf_results, stdev, mrange, num_mofs)
-#           --> returns a set of csv files to the folder 'saved_element_pmfs'
-#     calculate_kld(gases, list_of_arrays, bins, binned_probabilities)
-#           --> return(array_kld_results)
-#     choose_arrays(gases, num_mofs, array_kld_results, num_best_worst)
-#           --> return(best_and_worst_arrays_by_jointKLD,
-#               best_and_worst_arrays_by_gasKLD, best_ranked_by_product)
+from mof_array.pmf.process_mass_data import (
+    read_data_as_dict,
+    write_data_as_tabcsv,
+    yaml_loader,
+    import_experimental_data,
+    import_simulated_data,
+    calculate_element_pmf,
+    calculate_all_arrays,
+    create_bins,
+    bin_compositions,
+    calculate_kld,
+    choose_arrays,
+    assign_array_ids,
+    save_element_pmf_data,
+    save_unbinned_array_pmf_data,
+    save_binned_array_pmf_data,
+    plot_binned_array_pmf_data)
 
 # --------------------------------------------------
 # ----- Import RASPA Data and yaml File ------------
