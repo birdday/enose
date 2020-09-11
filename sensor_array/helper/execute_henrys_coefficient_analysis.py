28003594488bccdd315b5249f9dd89428ae84b30
birdday <22bday@gmail.com>
2020-09-11 15:59:52 -0400

    Removed previous analysis for individual and multiple ratios, as this is not consistent with a to-be-implemented data structure.
    N.B. This commit is atomic in that all remaining functions perfrom as intended, it just intentionally removes all code from the execute file.

--- sensor_array/helper/execute_henrys_coefficient_analysis.py
+++ sensor_array/helper/execute_henrys_coefficient_analysis.py
@@ -1,23 +1,3 @@
-# ----- Perform Analysis -----
-gases = ['acetone', 'ammonia', 'argon', 'CO2', 'hydrogen']
-# gases = ['CO2']
-air_ratios = ['3to1air', '4to1air', '5to1air']
-for gas in gases:
-    # Single Ratios Test
-    # for air_ratio in air_ratios:
-    #     folder = gas+'_'+air_ratio+'/'
-    #     sim_results_path = '/Users/brian_day/Desktop/HC_Work/HenrysConstants_Final/'+folder
-    #     analysis_results_path = '/Users/brian_day/Desktop/HC_Work/HenrysConstants_Analysis_Results/'+folder
-    #     if os.path.isdir(analysis_results_path) != True:
-    #         os.mkdir(analysis_results_path)
-    #     analyze_single_ratio(gas, air_ratio, sim_results_path, analysis_results_path)
-
-    # All Ratio Test
-    sim_results_path = '/Users/brian_day/Desktop/HC_Work/HenrysConstants_Final/'
-    analysis_results_path = '/Users/brian_day/Desktop/HC_Work/HenrysConstants_Analysis_Results/'+str(gas)+'_AllRatios/'
-    if os.path.isdir(analysis_results_path) != True:
-        os.mkdir(analysis_results_path)
-    analyze_all_ratios(gas, sim_results_path, analysis_results_path, hg_eval_type='R2', air_eval_type='With hg', r2_min_hg=0.95, r2_min_air=0.0, rmse_min_hg=0.10, rmse_min_air=0.10)
 
 gases = ['acetone', 'ammonia', 'argon', 'CO2', 'hydrogen']
 
