47949d962a7f252e65d1540d5eede44bd1ad69a7
birdday <22bday@gmail.com>
2020-09-14 10:25:45 -0400

    Updated all analysis functions to work with new data structures.

--- sensor_array/helper/henrys_coefficient_analysis.py
+++ sensor_array/helper/henrys_coefficient_analysis.py
@@ -1,6 +1,4 @@
-import glob
 import numpy as np
-import os
 import yaml
 import pandas as pd
 
@@ -16,261 +14,120 @@ def import_simulated_data(filename):
     return ads_data
 
 
+def create_weight_matrix(error, weight_type='error'):
+    if weight_type == None:
+        weights=np.ones(len(error))
+
+    elif weight_type == 'error':
+        # N.B. Connot simply invert value as can get a 0 error when adsorption is 0.
+        # Same is true for squared error.
+        weights_temp = [1/val for val in error if val != 0]
+        if len(weights_temp) != 0:
+            weights = np.array([1/val if val != 0 else max(weights_temp) for val in error])
+        else:
+            weights = np.ones(len(error))
+
+    elif weight_type == 'error_squared':
+        error_squared = [val**2 for val in error]
+        weights_temp = [1/val for val in error_squared if val != 0]
+        if len(weights_temp) != 0:
+            weights = np.array([1/val if val != 0 else max(weights_temp) for val in error_squared])
+        else:
+            weights = np.ones(len(error_sqaured))
+
+    else:
+        raise NameError('Invalid Error Type!')
+
+    return weights
 
 
+def calculate_r2_and_rmse(p, x_data, y_data):
+    fit = np.poly1d(p)
+    predicted_vals = fit(x_data)
+    residuals = predicted_vals-y_data
+    ybar = np.mean(y_data)
+    ss_res = np.sum(residuals**2)
+    ss_tot = np.sum((y_data-ybar)**2)
+    r2 = 1-(ss_res/ss_tot)
+    mse = ss_res/len(y_data)
+    rmse = mse**(0.5)
 
-def calculate_kH(comps, mass, error, eval_type='R2', r2_min=0.99, rmse_min=0.10, weight='error', flipped=False, i_offset=0, counter=1):
+    return r2, rmse, ybar
 
-    # Make sure data is properly sorted
-    indicies = np.argsort(comps)
-    comps_sorted = [comps[i] for i in indicies]
-    mass_sorted = [mass[i] for i in indicies]
-    error_sorted = [error[i] for i in indicies]
-    comps = comps_sorted
-    mass = mass_sorted
-    error = error_sorted
 
-    if flipped == True:
-        print('Flipped!')
-        comps = np.flipud(comps)
-        mass = np.flipud(mass)
-        error = np.flipud(error)
+def check_r2_and_rmse(r2, r2_min, rmse, rmse_min, y_bar, eval_type=None):
+    if eval_type == None:
+        return True
 
-    for i in range(len(comps)-i_offset,-counter,-1):
+    elif eval_type == 'R2':
+        if r2 >= r2_min:
+            return True
+        else:
+            return False
+
+    elif eval_type == 'RMSE':
+        if rmse <= rmse_min*y_bar:
+            return True
+        else:
+            return False
+
+    elif eval_type == 'Either':
+        if r2 >= r2_min or rmse <= rmse_min*y_bar:
+            return True
+        else:
+            return False
+
+    elif eval_type == 'Both':
+        if r2 >= r2_min and rmse <= rmse_min*y_bar:
+            return True
+        else:
+            return False
+
+    else:
+        raise NameError('Invalid eval_type!')
+
+
+def calculate_kH(ads_data, gas, eval_type='R2', r2_min=0.99, rmse_min=0.10, weight_type='error', fixed_intercept=False):
+
+    comps = ads_data[gas+'_comp']
+    mass = ads_data[gas+'_mass']
+    error = ads_data[gas+'_error']
+
+    offset = 0
+    for i in range(len(comps)-offset,0,-1):
 
         # Check if enough points for a proper fit
-        if i <= 2:
-            print('Could not fit to given data!')
-            return None, 0, 0, 0, 0, 0
+        # Need to adjust this to stop before all points have same x value. Okay for now though.
+        if i <= 6:
+            return [None, None], None, None, None, None
 
-        # Create a weighting matrix
-        if weight == 'None':
-            weights=np.ones(len(error[0:i]))
-        elif weight == 'error':
-            if 0 not in error[0:i]:
-                weights=np.divide(1,error[0:i])
-            else:
-                weights_temp = [1/val for val in error[0:i] if val != 0]
-                if len(weights_temp) != 0:
-                    weights = np.array([1/val if val != 0 else max(weights_temp) for val in error[0:i]])
-                else:
-                    weights = np.ones(len(error[0:i]))
-        elif weight == 'error_squared':
-            error_squared = []
-            for j in error[0:i]:
-                error_squared.extend([j**2])
-            if 0 not in error_sqaured:
-                weights = np.divide(1,error_squared)
-            else:
-                weights_temp = [1/val for val in error_squared if val != 0]
-                if len(weights_temp) != 0:
-                    weights = np.array([1/val if val != 0 else max(weights_temp) for val in error_squared])
-                else:
-                    weights = np.ones(len(error_sqaured[0:i]))
+        weights = create_weight_matrix(error[0:i], weight_type=weight_type)
+        if fixed_intercept == False:
+            p = np.polyfit(comps[0:i], mass[0:i], 1, cov=False, w=weights)
+        if fixed_intercept == True:
+            x = np.array(comps[0:i])[:,np.newaxis] * np.sqrt(weights[:,np.newaxis])
+            y = np.array(mass[0:i]) * np.sqrt(weights)
+            a, _, _, _ = np.linalg.lstsq(x, y, rcond=None)
+            p = [a, 0]
+        r2, rmse, ybar = calculate_r2_and_rmse(p, comps[0:i], mass[0:i])
+        calculate_r2_and_rmse(p, comps, mass)
+        if check_r2_and_rmse(r2, r2_min, rmse, rmse_min, ybar, eval_type=eval_type) == False:
+            offset += 1
         else:
-            raise NameError('Invalid Error Type!')
-
-        # Fit a Line
-        # p, V = np.polyfit(comps[0:i], mass[0:i], 1, cov=True, w=weights)
-        p = np.polyfit(comps[0:i], mass[0:i], 1, cov=False, w=weights)
-
-        # Calculate R_Squared and RMSE
-        fit = np.poly1d(p)
-        predicted_vals = fit(comps[0:i])
-        residuals = predicted_vals-mass[0:i]
-        ybar = np.sum(mass[0:i])/len(mass[0:i])
-        ss_res = np.sum(residuals**2)
-        ss_tot = np.sum((mass[0:i]-ybar)**2)
-        R2 = 1-(ss_res/ss_tot)
-        MSE = ss_res/len(mass[0:i])
-        RMSE = MSE**(0.5)
-
-        max_comp = comps[i-1]
-
-        # Check R_Squared and RMSE
-        if eval_type == None:
-            break
-        elif eval_type == 'R2':
-            if R2 >= r2_min:
-                break
-        elif eval_type == 'RMSE':
-            if RMSE <= rmse_min*ybar:
-                break
-        elif eval_type == 'Either':
-            if R2 >= r2_min or RMSE <= rmse_min*ybar:
-                break
-        elif eval_type == 'Both':
-            if R2 >= r2_min and RMSE <= rmse_min*ybar:
-                break
-        else:
-            raise NameError('Invalid eval_type!')
-
-    return p[0], p[1], max_comp, R2, RMSE, len(comps)-i
-
-
-def calculate_kH_alt(comps, mass, error, r2_min=0.99, weight='error', flipped=False):
-
-    # Make sure data is properly sorted
-    indicies = np.argsort(comps)
-    comps_sorted = [comps[i] for i in indicies]
-    mass_sorted = [mass[i] for i in indicies]
-    error_sorted = [error[i] for i in indicies]
-    comps = comps_sorted
-    mass = mass_sorted
-    error = error_sorted
-
-    if flipped == True:
-        print('Flipped!')
-        comps = np.flipud(comps)
-        mass = np.flipud(mass)
-        error = np.flipud(error)
-
-    for i in range(len(comps),-1,-1):
-
-        # Check if enough points for a proper fit
-        if i <= 2:
-            print('Could not fit to given data!')
-            return None, 0, 0, 0
-
-        # Linear Fit
-        if weight == 'None':
-            weights=np.ones(len(error[0:i]))
-        elif weight == 'error':
-            if 0 not in error[0:i]:
-                weights=np.divide(1,error[0:i])
-            else:
-                weights_temp = [1/val for val in error[0:i] if val != 0]
-                if len(weights_temp) != 0:
-                    weights = np.array([1/val if val != 0 else max(weights_temp) for val in error[0:i]])
-                else:
-                    weights = np.ones(len(error[0:i]))
-        elif weight == 'error_squared':
-            error_squared = []
-            for j in error[0:i]:
-                error_squared.extend([j**2])
-            if 0 not in error_sqaured:
-                weights = np.divide(1,error_squared)
-            else:
-                weights_temp = [1/val for val in error_squared if val != 0]
-                if len(weights_temp) != 0:
-                    weights = np.array([1/val if val != 0 else max(weights_temp) for val in error_squared])
-                else:
-                    weights = np.ones(len(error_sqaured[0:i]))
-        else:
-            raise NameError('Invalid Error Type!')
-
-        x = np.array(comps[0:i])
-        x = np.vstack([x, np.ones(len(x))]).T
-        y = np.array(mass[0:i])
-
-        xw = x*np.sqrt(weights[:,np.newaxis])
-        yw = y*np.sqrt(weights)
-        a, b, c, d = np.linalg.lstsq(xw, yw, rcond=None)
-
-        # Calculate R_Squared
-        fit = np.poly1d(a)
-        predicted_vals = fit(comps[0:i])
-        residuals = predicted_vals-mass[0:i]
-        ybar = np.sum(mass[0:i])/len(mass[0:i])
-        ss_res = np.sum(residuals**2)
-        ss_tot = np.sum((mass[0:i]-ybar)**2)
-        R2 = 1-(ss_res/ss_tot)
-
-        max_comp = comps[i-1]
-
-        # Check R_Squared
-        # print('i = '+str(i)+'\t R^2 = '+str(R2))
-        if R2 >= r2_min:
-            # print('Number of Points Used: ', len(comps[0:i]))
-            # print('R^2 = ', R2)
-            # print('Henry Coeff. = ', p[0])
-            # print('Intercept (should be 0) = ',p[1])
-            max_comp = comps[i-1]
             break
 
-    return a[0], a[1], R2, max_comp
+    max_comp = comps[i-1]
+    return p, max_comp, r2, rmse, i
 
 
-def calculate_kH_fixed_intercept(comps, mass, error, eval_type='R2', r2_min=0.99, rmse_min=0.10, weight='error', counter=1):
+def calculate_kH_air(ads_data, gas, i, weight_type='error'):
 
-    # Make sure data is properly sorted
-    indicies = np.argsort(comps)
-    comps_sorted = [comps[i] for i in indicies]
-    mass_sorted = [mass[i] for i in indicies]
-    error_sorted = [error[i] for i in indicies]
-    comps = comps_sorted
-    mass = mass_sorted
-    error = error_sorted
+    comps = ads_data[gas+'_comp']
+    mass = ads_data['O2_mass']+ads_data['N2_mass']
+    error = ads_data['O2_error']+ads_data['N2_error']
 
-    for i in range(len(comps),-counter,-1):
+    weights = create_weight_matrix(error[0:i], weight_type=weight_type)
+    p = np.polyfit(comps[0:i], mass[0:i], 1, cov=False, w=weights)
+    r2, rmse, ybar = calculate_r2_and_rmse(p, comps[0:i], mass[0:i])
 
-        # Check if out of points
-        if i <= 2:
-            print('Could not fit to given data!')
-            return None, 0, 0, 0, 0
-
-        # Linear Fit
-        if weight == 'None':
-            weights=np.ones(len(error[0:i]))
-        elif weight == 'error':
-            if 0 not in error[0:i]:
-                weights=np.divide(1,error[0:i])
-            else:
-                weights_temp = [1/val for val in error[0:i] if val != 0]
-                if len(weights_temp) != 0:
-                    weights = np.array([1/val if val != 0 else max(weights_temp) for val in error[0:i]])
-                else:
-                    weights = np.ones(len(error[0:i]))
-        elif weight == 'error_squared':
-            error_squared = []
-            for j in error[0:i]:
-                error_squared.extend([j**2])
-            if 0 not in error_sqaured:
-                weights = np.divide(1,error_squared)
-            else:
-                weights_temp = [1/val for val in error_squared if val != 0]
-                if len(weights_temp) != 0:
-                    weights = np.array([1/val if val != 0 else max(weights_temp) for val in error_squared])
-                else:
-                    weights = np.ones(len(error_sqaured[0:i]))
-        else:
-            raise NameError('Invalid Error Type!')
-
-        x = np.array(comps[0:i])
-        x = x[:,np.newaxis]
-        y = np.array(mass[0:i])
-
-        xw = x*np.sqrt(weights[:,np.newaxis])
-        yw = y*np.sqrt(weights)
-        a, b, c, d = np.linalg.lstsq(xw, yw, rcond=None)
-
-        # Calculate R_Squared
-        fit = np.poly1d([a[0], 0])
-        predicted_vals = fit(comps[0:i])
-        residuals = predicted_vals-mass[0:i]
-        ybar = np.sum(mass[0:i])/len(mass[0:i])
-        ss_res = np.sum(residuals**2)
-        ss_tot = np.sum((mass[0:i]-ybar)**2)
-        R2 = 1-(ss_res/ss_tot)
-        MSE = ss_res/len(mass[0:i])
-        RMSE = MSE**(0.5)
-
-        max_comp = comps[i-1]
-
-        # Check R_Squared and RMSE
-        if eval_type == 'R2':
-            if R2 >= r2_min:
-                break
-        elif eval_type == 'RMSE':
-            if RMSE <= rmse_min*ybar:
-                break
-        elif eval_type == 'Either':
-            if R2 >= r2_min or RMSE <= rmse_min*ybar:
-                break
-        elif eval_type == 'Both':
-            if R2 >= r2_min and RMSE <= rmse_min*ybar:
-                break
-        else:
-            raise NameError('Invalid eval_type!')
-
-    return a[0], max_comp, R2, RMSE, len(comps)-i
+    return p, r2, rmse
