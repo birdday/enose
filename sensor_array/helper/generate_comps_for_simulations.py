15b84e092eb6b939831db65f955ef13c89378152
birdday <22bday@gmail.com>
2020-09-17 12:35:32 -0400

    Created config file for generation simulation compositions and added yaml loader and execute function to corresponding script.

--- sensor_array/helper/generate_comps_for_simulations.py
+++ sensor_array/helper/generate_comps_for_simulations.py
@@ -1,5 +1,13 @@
 import numpy as np
 import csv
+import yaml
+
+
+def yaml_loader(filepath):
+    with open(filepath, 'r') as yaml_file:
+        data = yaml.load(yaml_file)
+    return data
+
 
 def create_henrys_comp_list(henrys_gases, henrys_comps, background_gases, background_gas_ratios, filename=None):
     bg_gas_total = np.sum(background_gas_ratios)
@@ -19,3 +27,14 @@ def create_henrys_comp_list(henrys_gases, henrys_comps, background_gases, backgr
             writer.writerows(comp_list)
 
     return comp_list
+
+
+def execute_create_henrys_comp_list(config_file):
+        data = yaml_loader(config_file)
+        trace_gases = data['trace_gases']
+        trace_gas_comps = data['trace_gas_comps']
+        background_gases = list(data['background_gases'].keys())
+        background_gas_ratios = [data['background_gases'][gas]['Value'] for gas in background_gases]
+        filename = data['filename']
+        for gas in background_gases:
+            create_henrys_comp_list([gas], trace_gas_comps, background_gases, background_gas_ratios, filename=filename)
