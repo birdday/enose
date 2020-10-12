ca7bbeeb11bfcf6f21c1ab43e3f52fe065d05029
birdday <22bday@gmail.com>
2020-10-12 10:33:39 -0400

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+setup(
+    name = 'Sensor_Array',
+    version = '0.1.0',
+    description = 'Simulation and Analysis of MOF-based gas sensor arrays',
+    author = 'Brian A Day, Jenna A Gustafson',
+    author_email = 'brd84@pitt.edu',
+    url = 'https://github.com/birdday/Sensor_Array',
+    packages = ['sensor_array'],
+    install_requires=[
+        'numpy',
+        'scipy',
+        'click',
+        'pandas',
+        'pyyaml',
+        'matplotlib',
+	'tensorflow',
+    ],
+    entry_points={
+          'console_scripts': [
+          ]
+      },
+)
