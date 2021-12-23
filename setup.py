97d27f994a8504ce771951d749c4610b3e7f4c2a
Brian Day <22bday@gmail.com>
2021-12-23 02:48:19 -0500

    Fixed package name in setup.py.

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 
 from distutils.core import setup
 setup(
-    name = 'Sensor_Array',
+    name = 'eNose',
     version = '0.1.0',
     description = 'Simulation and Analysis of MOF-based gas sensor arrays',
     author = 'Brian A Day, Jenna A Gustafson',
     author_email = 'brd84@pitt.edu',
-    url = 'https://github.com/birdday/Sensor_Array',
-    packages = ['sensor_array'],
+    url = 'https://github.com/birdday/enose',
+    packages = ['enose'],
     install_requires=[
         'numpy',
         'scipy',
